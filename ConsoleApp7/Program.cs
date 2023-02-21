a944670ebd4d989cbed20b0e54ae257a5a6da369
Valerio Valdes <valerio.valdes@dev4side.com>
2023-02-21 13:19:43 +0100

    esercizio biblioteca

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -5,9 +5,11 @@ using System.ComponentModel;
 using System.Data.Common;
 using System.Linq;
 using System.Net.Http;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Security.AccessControl;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConsoleApp7
@@ -16,67 +18,50 @@ namespace ConsoleApp7
     {
         static void Main(string[] args)
         {
-
             /*
-             agenzia: {immobili{codice, tipologia, prezzo, città, superficie}}
-             mostraImmobili(string ordinatiPer [codice, prezzo,città,superficie])
-             aggiungiImmobile(codice, tipologia, prezzo, città, superficie) //CODICE DEVE ESSERE UNIVOCO
-             rimuoviImmobile()
-            getImmobilePiùCaro()
-            getImmobilePiùGrande()
-            getValoreTotaleImmobili()
+            progettare un software per la gestione di una biblioteca
+            gli utenti fornendo nome cognome email password possono effettuare dei prestiti
+            libri, DVD
+            libro{Codice, titolo, anno, genere, stato (in prestito, disponibile), Autori Lista di autori, numPagine}
+            DVD  {Codice, titolo, anno, genere, stato (in prestito, disponibile),Autori Lista di autori, durata}
+            l'utente può cercare per titolo o per codice sia i dvd che i libri
+            l'utente può prendere in prestito un elemento (libro o DVD)
              */
-            var l = new List<Autore>
-            {
-                new Autore { Cognome = "rossi", Nome = "Mario", Codice = "rm1" },
-                new Autore { Cognome = "verdi", Nome = "Mario", Codice = "vm1" },
-                new Autore { Cognome = "rossi", Nome = "Luca", Codice = "rl1" },
-                new Autore { Cognome = "Bianchi", Nome = "Gino", Codice = "bg1" },
-                new Autore { Cognome = "blu", Nome = "Andrea", Codice = "ba1" }
-            };
-            
-            
-            //var l2 = new List<Autore>();
-            //l2.Add(l[0]);
-            l.Remove(new Autore());
-            string searchString = Console.ReadLine();
-            //action, predicate, func
-            Func<int, int, string> somma = (n1, n2) => { return (n1 + n2).ToString(); };
-            List<Autore> marios = l.FindAll((f) => f.Nome == searchString);
-
-            if (l.Exists(a => a.Nome == "mario"))
-            {
-                Console.WriteLine("exist: esiste almeno un mario");
-            }
-            //LINQ
-            if (l.Any(a => a.Nome == "mario"))
-            {
-                Console.WriteLine("any: esiste almeno un mario");
-            }
-
-            Console.WriteLine(l.Count);
-            Console.ReadLine();
         }
     }
 
-    public class Catalogo<T>
+    public class Persona
     {
-        private List<T> _elenco = new List<T>();
-
-        public void Add(T item)
+        protected string Nome { get; set; }
+        public string Cognome { get; set; }
+        public Persona(string nome, string cognome)
         {
-            _elenco.Add(item);
+            Nome = nome;
+            Cognome = cognome;
         }
 
-        public int GetCount()
+        public override string ToString()
         {
-            return _elenco.Count;
+            return $"{Nome} {Cognome}";
         }
 
-        public T GetFirst()
+        public virtual void Saluta()
         {
-            return _elenco.FirstOrDefault();
+            Console.WriteLine($"Ciao sono una persona e mi chiamo {Nome} {Cognome}");
         }
+    }
+
+    public class Studente : Persona
+    {
+        public Studente(string nome, string cognome) : base(nome, cognome)
+        {
+        }
+
+        public override void Saluta()
+        {
+            Console.WriteLine($"Ciao sono uno studente e mi chiamo {Nome} {Cognome}");
+        }
+
     }
 
 
