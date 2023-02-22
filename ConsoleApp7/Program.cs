edbc44dc3d7b19e99447876580391693d255bbd9
Valerio Valdes <valerio.valdes@dev4side.com>
2023-02-22 13:08:49 +0100

    Esercizio Gestione corsi

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -18,50 +18,109 @@ namespace ConsoleApp7
     {
         static void Main(string[] args)
         {
-            /*
-            progettare un software per la gestione di una biblioteca
-            gli utenti fornendo nome cognome email password possono effettuare dei prestiti
-            libri, DVD
-            libro{Codice, titolo, anno, genere, stato (in prestito, disponibile), Autori Lista di autori, numPagine}
-            DVD  {Codice, titolo, anno, genere, stato (in prestito, disponibile),Autori Lista di autori, durata}
-            l'utente può cercare per titolo o per codice sia i dvd che i libri
-            l'utente può prendere in prestito un elemento (libro o DVD)
-             */
+            var l = new List<Forma>();
+            l.Add(new Rettangolo() { Altezza = 10, Base = 20 });
+            l.Add(new Cerchio() { Raggio = 3 });
+            l.Add(new Quadrato() { Lato = 5 });
+/*
+Si vuole realizzare un sistema per la gestione di Corsi. 
+
+Ciascun corso ha un nome e un numero intero che indica l’edizione. 
+Ciascun corso si articola in un certo numero di lezioni. 
+Ogni lezione ha una descrizione, una data, un orario di inizio, una durata, un docente e un’aula assegnata.
+
+I Docenti hanno un nome, un cognome e un titolo di studio.
+
+Ogni Aula ha una capienza, un nome e un elenco di risorse (es. Video Proiettore, PC, Notebook, Tablet, LIM, etc.).
+
+Ogni corso ha un certo numero di studenti partecipanti, iscritti a quella specifica edizione. 
+Ogni studente ha un nome, un cognome e una matricola. 
+Per ogni lezione occorre tenere traccia dei presenti.
+
+Il sistema deve consentire:
+Aggiungere corso
+Aggiungere lezioni ad un corso
+Aggiungere studenti ad un corso
+Segnare gli assenti ad una lezione
+
+Il sistema deve consentire le seguenti stampe a video:
+Elenco Corsi
+Elenco delle lezioni di un corso
+Elenco degli iscritti a un corso
+Scheda riassuntiva di una lezione
+Elenco dei presenti ad una lezione
+*/
         }
     }
 
-    public class Persona
+    public abstract class Forma
     {
-        protected string Nome { get; set; }
-        public string Cognome { get; set; }
-        public Persona(string nome, string cognome)
+        public virtual void Disegna()
         {
-            Nome = nome;
-            Cognome = cognome;
+            Console.WriteLine("Sto disegnando una forma generica");
+        }
+        public virtual void StampaDimensioni()
+        {
+            Console.WriteLine($"{Environment.NewLine}Le dimensioni dell'oggetto sono:");
+        }
+        public abstract double GetPerimetro();
+    }
+    public class Rettangolo : Forma
+    {
+        public int Altezza { get; set; }
+        public int Base { get; set; }
+        public override void Disegna()
+        {
+            Console.WriteLine("Sto disegnando un rettangolo");
         }
 
-        public override string ToString()
+        public override double GetPerimetro()
         {
-            return $"{Nome} {Cognome}";
+            return Altezza * 2 + Base * 2;
         }
 
-        public virtual void Saluta()
+        public virtual void StampaDimensioni()
         {
-            Console.WriteLine($"Ciao sono una persona e mi chiamo {Nome} {Cognome}");
+            base.StampaDimensioni();
+            Console.WriteLine($"Altezza: {Altezza} {Environment.NewLine}Base: {Base}");
         }
     }
-
-    public class Studente : Persona
+    public class Cerchio : Forma
     {
-        public Studente(string nome, string cognome) : base(nome, cognome)
+        public int Raggio { get; set; }
+        public override void Disegna()
         {
+            Console.WriteLine("Sto disegnando un cerchio");
         }
 
-        public override void Saluta()
+        public override double GetPerimetro()
         {
-            Console.WriteLine($"Ciao sono uno studente e mi chiamo {Nome} {Cognome}");
+            return Math.Pow(Raggio, 2) * Math.PI;
         }
 
+        public override void StampaDimensioni()
+        {
+            base.StampaDimensioni();
+            Console.WriteLine($"Raggio: {Raggio}");
+        }
+    }
+    public class Quadrato : Rettangolo
+    {
+        public int Lato { get; set; }
+        public override void Disegna()
+        {
+            Console.WriteLine("Sto disegnando un quadrato");
+        }
+        public override void StampaDimensioni()
+        {
+            base.StampaDimensioni();
+            Console.WriteLine($"Lato: {Lato}");
+        }
+
+        public override double GetPerimetro()
+        {
+            return Lato * 4;
+        }
     }
 
 
