2f88e803cdd6b7eed3c44309e38d3b22e372a60d
Valerio Valdes <valerio.valdes@dev4side.com>
2023-02-24 14:01:41 +0100

    esercizio csv

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -14,25 +14,21 @@ namespace ConsoleApp7
     {
         static void Main(string[] args)
         {
-            var c = new Calcolatrice();
-            try
-            {
-                int divisione = c.GetDivisione();
-                Console.WriteLine(divisione);
-                Console.WriteLine("divisione finita");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("sono andato in errore");
-                using(var sw = new StreamWriter("C:\\Users\\Valerio\\Source\\Repos\\ConsoleApp7\\error.log", true))
-                {
-                    sw.WriteLine(DateTime.Now.ToLongTimeString());
-                    sw.WriteLine(ex.Message);
-                    sw.WriteLine(ex.StackTrace);
-                }
-            }
-            
-            Console.ReadLine();
+            /*
+In un file sono riportati i consumi (in kWh o in Wh) per la ricarica di auto elettriche distinte per utente (cognome, nome), 
+            data (gg/mm/yyyy hh:mm:ss). Si ipotizzi che i dati del file siano relativi ad un mese.
+
+Il tracciato è il seguente:
+Cognome;Nome;Consumo;UnitàMisura;Data
+
+Scrivere un programma che:
+a) metodo per popolare il csv
+1) carichi in una opportuna struttura dati in memoria i dati del file
+2) dato il cognome e nome di un utente determini il consumo totale e il costo in €. 
+            Il costo del kWh è letto come parametro in input (valore realistici sono 0,06243 €/kWh).
+3) ripetere il punto 2) limitandolo ad un giorno della mese. Il giorno è un numero intero letto in input.
+4) aggregare i dati per giorno, e trovare il consumo totale in kWh e il costo totale in €.         
+             */
         }
     }   
 }
