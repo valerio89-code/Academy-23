0d649f4f571b5d63337984861b6f060470d7bbc0
Valerio Valdes <valerio.valdes@dev4side.com>
2023-03-02 13:53:44 +0100

    argomenti

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -9,34 +9,25 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using System.Collections;
 using System.Linq.Expressions;
+using System.Threading;
 
 namespace ConsoleApp7
 {
-
     internal static class Program
     {
         static void Main(string[] args)
         {
-            var campColosseo = new Campanello() { Name = "colosseo" };
-            var campDuomo = new Campanello() { Name = "duomo di milano" };
-            var campCasaValerio = new Campanello() { Name = "casa di valerio" };
-
-            var l1 = new Ladro() { Nome = "michele", Specialità  = "serrature"};
-            var l2 = new Ladro() { Nome = "luca", Specialità = "casseforti" };
-            var l3 = new Ladro() { Nome = "gianna", Specialità = "scassinature" };
-
-            campDuomo.RingPressed += l1.CheckPorta;
-            campDuomo.RingPressed += l2.CheckPorta;
-
-            campColosseo.RingPressed += l3.CheckPorta;
-
-
-
-            campColosseo.OnRingPressed();
-            campDuomo.OnRingPressed();
-            campCasaValerio.OnRingPressed();
-
-
+            //n! 5 1*2*3*4*5
+            //Fact(5)
+            Console.WriteLine(Fact(5));
         }
+
+        public static int Fact(int n)
+        {
+            //caso base
+            if (n == 1) return 1;
+            return n * Fact(n - 1);
+        }
+
     }
 }
